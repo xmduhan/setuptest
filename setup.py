a5a6e1697529839fc090cc50a14a920cf8986002
xmduhan <xmduhan@gmail.com>
2015-02-20 15:42:03 +0800

    ...

--- setup.py
+++ setup.py
@@ -5,3 +5,13 @@ Created on Fri Feb 20 15:21:59 2015
 @author: Administrator
 """
 
+from distutils.core import setup
+
+setup(name='setuptest',
+      version='1.0',
+      description='setuptest',
+      author='xmduhan',
+      author_email='xmduhan@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
