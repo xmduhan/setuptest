234f0d0215145a1b748b06fe37aeec3b05d7e158
xmduhan <xmduhan@gmail.com>
2015-02-20 15:59:18 +0800

    ...

--- setup.py
+++ setup.py
@@ -4,14 +4,21 @@ Created on Fri Feb 20 15:21:59 2015
 
 @author: Administrator
 """
+#%%
+#from setuptools import find_packages, setup
+#%%
+#setup(name='setuptest',
+#      version='1.0',
+#      description='setuptest',
+#      author='xmduhan',
+#      author_email='xmduhan@gmail.com',
+#      url='https://www.python.org/sigs/distutils-sig/',
+#      packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+#     )
 
-from distutils.core import setup
-
-setup(name='setuptest',
-      version='1.0',
-      description='setuptest',
-      author='xmduhan',
-      author_email='xmduhan@gmail.com',
-      url='https://www.python.org/sigs/distutils-sig/',
-      #packages=['setuptest'],
-     )
+from setuptools import setup, find_packages
+setup(
+    name = "HelloWorld",
+    version = "0.1",
+    packages = find_packages(),
+)
