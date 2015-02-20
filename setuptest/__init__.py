6f3675f0c44485aef07aaffba1d53ec1811be86d
xmduhan <xmduhan@gmail.com>
2015-02-20 16:40:23 +0800

    ...

---
+++ setuptest/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 20 15:21:31 2015
+
+@author: Administrator
+"""
+
---
+++ setuptest/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 20 15:20:12 2015
+
+@author: Administrator
+"""
+
+
+def sayHello():
+    '''
+    '''
+    print 'hello package!'
